4fde19b4b77c80b5d36376a763f644fc3711d1e2
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-06-26 14:45:21 -0700

    bugfix: Add Missing Requirement

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    'clowder'
+    'clowder',
+    'isodate'
 ]
 
 test_requirements = [
